0d5c6ffcfb9cd070678e290c7d2f4be34fd5daab
saul <saul.maita@hotmail.com>
2011-12-22 14:52:22 -0500

    Initial commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,8 @@
+class WelcomeController < ApplicationController
+  def index
+  end
+  
+  def saul
+        render_text "no fue tan facil"
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Recetario::Application.config.secret_token = 'd3fe3ea1c080f4a44677d78cd88f54d4ecf2e9d1e47254913ed668529c4e06d2da23f6f5044c9316566ea56318528f03b5ee83b61ce46e5b8e591debb4a3a530'
